1599483f9cbaea4ecc31b370c0665ce26422cf66
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-01-10 16:58:47 +0100

    Update `nextclade3` to alpha.2
    See https://github.com/nextstrain/nextclade/releases/tag/3.0.0-alpha.2

--- src/build.sh
+++ src/build.sh
@@ -6,5 +6,5 @@ mkdir -p "$PREFIX"/bin
 cp -v "$SRC_DIR"/envdir "$PREFIX"/bin/envdir
 
 # Nextclade v3
-curl -fsSL -o "$PREFIX"/bin/nextclade3 https://github.com/nextstrain/nextclade/releases/download/3.0.0-alpha.1/nextclade-$("$SRC_DIR"/target-triple)
+curl -fsSL -o "$PREFIX"/bin/nextclade3 https://github.com/nextstrain/nextclade/releases/download/3.0.0-alpha.2/nextclade-$("$SRC_DIR"/target-triple)
 chmod a+rx "$PREFIX"/bin/nextclade3
