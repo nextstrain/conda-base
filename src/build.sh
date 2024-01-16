c861f00a2bc278d9597b0f1e4b491ff046a3a733
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-01-16 17:19:58 +0100

    Transition nextclade 2->3
    Update nextclade3 version
    
    Add nextclade2 in build.sh for stability until bioconda update is sorted out

--- src/build.sh
+++ src/build.sh
@@ -6,5 +6,11 @@ mkdir -p "$PREFIX"/bin
 cp -v "$SRC_DIR"/envdir "$PREFIX"/bin/envdir
 
 # Nextclade v3
-curl -fsSL -o "$PREFIX"/bin/nextclade3 https://github.com/nextstrain/nextclade/releases/download/3.0.0-alpha.2/nextclade-$("$SRC_DIR"/target-triple)
+# TODO: Remove once bioconda has nextclade3
+curl -fsSL -o "$PREFIX"/bin/nextclade3 https://github.com/nextstrain/nextclade/releases/download/3.0.0/nextclade-$("$SRC_DIR"/target-triple)
 chmod a+rx "$PREFIX"/bin/nextclade3
+
+# Nextclade v2
+# TODO: Remove once nextclade2 package is added to recipe.yaml
+curl -fsSL -o "$PREFIX"/bin/nextclade2 https://github.com/nextstrain/nextclade/releases/download/2.14.0/nextclade-$("$SRC_DIR"/target-triple)
+chmod a+rx "$PREFIX"/bin/nextclade2
