74eeda56d12662b7fee0d9b540dc0ad94e0e22b4
Victor Lin <13424970+victorlin@users.noreply.github.com>
2025-08-07 15:26:41 -0700

    🚧 use branch with augur subsample

--- src/build.sh
+++ src/build.sh
@@ -2,5 +2,8 @@
 set -euo pipefail -x
 mkdir -p "$PREFIX"/bin
 
+# Install augur from git branch
+pip install git+https://github.com/nextstrain/augur.git@victorlin/subsample
+
 # envdir
 cp -v "$SRC_DIR"/envdir "$PREFIX"/bin/envdir
